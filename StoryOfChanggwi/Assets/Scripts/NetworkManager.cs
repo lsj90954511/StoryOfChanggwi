5613c1b627bd22e30cb3edb576ab53d2ca769c87
Sojin Lee <31687412+lsj90954511@users.noreply.github.com>
2023-10-23 23:06:19 +0900

    방 만들기까지 완료
    오류 수정, 방 만들고 GamePlay 씬으로 이동하는 것까지 완료

---
+++ StoryOfChanggwi/Assets/Scripts/NetworkManager.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using Photon.Pun;
+using Photon.Realtime;
+
+public class NetworkManager : MonoBehaviourPunCallbacks
+{
+    [SerializeField]
+    private TMP_InputField nicknameInputField;
+    [SerializeField]
+    private GameObject onlineUI;
+    [SerializeField]
+    private GameObject mainUI;
+
+    public void Connect()
+    {
+        PhotonNetwork.ConnectUsingSettings();
+
+        //닉네임 입력되지 않았을 경우 닉네임을 익명으로 설정
+        if (nicknameInputField.text == "")
+        {
+            nicknameInputField.text = "익명";
+        }
+        PhotonNetwork.LocalPlayer.NickName = nicknameInputField.text;
+
+        //OnlineUI로 넘어감
+        onlineUI.SetActive(true);
+        mainUI.SetActive(false);
+    }
+
+    //마스터 서버로 들어감
+    public override void OnConnectedToMaster()
+    {
+
+    }
+}
